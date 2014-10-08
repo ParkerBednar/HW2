e3113d4e543cafdd9e1137e26b031d26910738a9
parkerbednar <pbednar14@gmail.com>
2014-10-08 18:28:51 -0300

    finished

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,34 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(params[:sort_by])
+    @all_ratings=Movie.ratings
+    @checked_boxes=@all_ratings    
+    
+    if params[:ratings]
+        @checked_boxes=params[:ratings]
+    elsif session[:ratings]
+        @checked_boxes=session[:ratings]
+        redirect=true
+    end
+    if params[:sort_by]
+        @sorting=params[:sort_by]
+    elsif session[:sort_by]
+        @sorting=session[:sort_by]
+        redirect=true
+    end
+    if redirect
+        redirect_to movies_path( :sort_by=>@sorting, :ratings=>@checked_boxes)
+    end
+    @movies = Movie.order(@sorting).find_all_by_rating(@checked_boxes.keys) 
+    if params[:sort_by] == 'title'
+        @title_header = "hilite"
+        @release_date_header = ''
+    elsif params[:sort_by] == 'release_date'
+	   @title_header = ""
+        @release_date_header = 'hilite'
+    end
+    session[:sort_by]=@sorting
+    session[:ratings]=@checked_boxes
   end
 
   def new
